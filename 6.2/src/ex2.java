25b324435dccda163fd16b9231b10b3ab42edc6c
Maria Pasca <32907490+MPasca@users.noreply.github.com>
2020-10-13 15:53:33 +0300

    problema6.2

---
+++ 6.2/src/ex2.java
@@ -0,0 +1,17 @@
+public class ex2 {
+	public static void main(String [] args) {
+		int i = Integer.parseInt("34");
+		System.out.println(i);
+
+		String s = String.valueOf(22);
+		System.out.println(s);
+
+		float n = Float.parseFloat("14.554321");
+		System.out.println(n);
+
+		float a = 6.2f;
+		System.out.println(a);
+		System.out.println("\n");
+	}
+	
+}
