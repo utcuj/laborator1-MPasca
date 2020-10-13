e30b55dd27dd74056497b0bd26ac4794efe178ef
Maria Pasca <32907490+MPasca@users.noreply.github.com>
2020-10-13 15:55:54 +0300

    problema 6.3

---
+++ 6.3/src/ex3.java
@@ -0,0 +1,19 @@
+public class ex3 {
+	public static void main(String [] args) {
+		int b = Integer.MAX_VALUE;
+		System.out.println(b);
+		System.out.println(b+1);
+		
+		int c = Integer.MIN_VALUE;
+		System.out.println(c);
+		System.out.println(c-1);
+		System.out.println("\n");
+		
+		float d = Float.MAX_VALUE;
+		System.out.println(d);
+		System.out.println((d+d)-(d+d));
+		
+		float e = 2.8f + 0.17f;
+		System.out.println(e);
+	}
+}
