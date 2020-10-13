4bfdc1a1553bbd9ac780748fcfa5b5c251cdc5b5
Maria Pasca <32907490+MPasca@users.noreply.github.com>
2020-10-13 16:01:19 +0300

    problema 6.4

---
+++ 6.4/src/ex4.java
@@ -0,0 +1,19 @@
+public class ex4 {
+	public static void main(String [] args) {
+		//int f = 56;
+		//System.out.println(f/0);
+		
+		double total = 0;
+		total += 5.6;
+		total += 5.8;
+		System.out.println(total);
+		
+		float g = -13.5f;
+		float h = 13.5f;
+		System.out.println(g/0);
+		System.out.println(h/0);		
+		
+		int j = (int)13.67f;
+		System.out.println(j);
+	}
+}
